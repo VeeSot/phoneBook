699dde4c6ffabb3838fbc66ca5a931c541b630fa
Vee Sot <veesot@devdrive.ru>
2016-04-03 12:22:28 +0500

    Add search for records

--- web/js/app.js
+++ web/js/app.js
@@ -1,9 +1,19 @@
 (function () {
     angular.module('phone_book', [])
         .controller('phone_book_controller', ['$scope', '$http', function ($scope, $http) {
+            $scope.query = '';
             $scope.records = [];
             $scope.getAllRecords = function () {
                 $http.get('/records').success(
+                    function (response) {
+                        $scope.records = response;
+                    })
+            };
+            //Search by substring
+            $scope.search = function () {
+                $http.get('/records', {
+                    params: {people: $scope.query}
+                }).success(
                     function (response) {
                         $scope.records = response;
                     })
