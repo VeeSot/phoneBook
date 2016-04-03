38430569bb5cadb880cf4e16f2cfbd2013ccf68f
Vee Sot <veesot@devdrive.ru>
2016-04-03 17:18:03 +0500

    Add functionality delete record from  phone book

--- web/js/app.js
+++ web/js/app.js
@@ -1,24 +1,28 @@
 (function () {
     angular.module('phone_book', [])
-        .controller('phone_book_controller', ['$scope', '$http', function ($scope, $http) {
+        .controller('phone_book_controller', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
             $scope.query = '';
             $scope.records = [];
             $scope.newRecord = {};
+
             $scope.getAllRecords = function () {
                 $http.get('/records').success(
                     function (response) {
-                        $scope.records = response;
+                        $scope.records = $filter('orderBy')(response, 'people');
                     })
             };
+
             //Search by substring
             $scope.search = function () {
                 $http.get('/records', {
                     params: {people: $scope.query}
                 }).success(
                     function (response) {
-                        $scope.records = response;
+                        $scope.records = $filter('orderBy')(response, 'people');
                     })
             };
+
+
             //Add new model to server
             $scope.addNewRecord = function () {
                 //Validation
@@ -36,15 +40,17 @@
                     console.log(response);
                     $.notify("Запись успешно добавлена в справочник", "success");
                     var newRecord = $scope.newRecord;
-                    newRecord['id'] = response.id;
+                    newRecord['recordId'] = response.data.id;
                     $scope.records.push(newRecord);
+                    $scope.records = $filter('orderBy')($scope.records, 'people');
                     $scope.newRecord = {}
                 }, function errorCallback(response) {
                     var data = response.data;
-                    if (data.id == 0 && data.message == "Contact exist!"){
+                    if (data.id == 0 && data.message == "Contact exist!") {
                         $.notify("Данная запись уже существует в справочнике", "error");
                     }
-                    else{
+                    else {
+                        console.log(response);
                         $.notify("Кажется что то поломалось.\nМы будем благодарны, если Вы сообщите об этом", "error");
                     }
 
@@ -52,8 +58,27 @@
                 });
 
             };
+
+
+            //Remove model on the server and local
+            $scope.removeRecord = function (idx) {
+                var recordToDelete = $scope.records[idx];
+                $http.delete('/records/' + recordToDelete.recordId).then(function successCallback(response) {
+                    if (response.status == 204) {
+                        $scope.records.splice(idx, 1);//Выкинем из списка моделей
+                        $.notify("Запись была успешно удалена из справочника", "success");
+                    }
+                }, function errorCallback(response) {
+                    console.log(response);
+                    $.notify("Кажется что то поломалось.\nМы будем благодарны, если Вы сообщите об этом", "error");
+                });
+
+            };
+
             $scope.init = function () {
                 $scope.getAllRecords();
             };
+
+
         }]);
 }());
