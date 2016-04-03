b6f5e0972b26b13c12605a1972b0cb45cd2b6fff
Vee Sot <veesot@devdrive.ru>
2016-04-03 11:52:56 +0500

    Add recive models from server-side

--- web/js/app.js
+++ web/js/app.js
@@ -1,1 +1,15 @@
-console.log("1");
+(function () {
+    angular.module('phone_book', [])
+        .controller('phone_book_controller', ['$scope', '$http', function ($scope, $http) {
+            $scope.records = [];
+            $scope.getAllRecords = function () {
+                $http.get('/records').success(
+                    function (response) {
+                        $scope.records = response;
+                    })
+            };
+            $scope.init = function () {
+                $scope.getAllRecords();
+            };
+        }]);
+}());
